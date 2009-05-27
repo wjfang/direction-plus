6d6ffc2fcf9d27d059c7f5b25c6b5dbaa05c1e7a
wjfang <wjfang@e0ac665e-29a9-11de-ae70-5144d73449a4>
2009-05-27 13:34:16 +0000

    Query without update.

---
+++ dplusapp/src/org/silentsquare/dplus/bbctnews/TestNewsDatabase.java
@@ -0,0 +1,77 @@
+package org.silentsquare.dplus.bbctnews;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Read + Serialization = 464328ms
+ * Deserialization 		= 125ms
+ * @author wjfang
+ *
+ */
+public class TestNewsDatabase extends AbstractNewsDatabase {
+	
+	private static final Logger logger = Logger.getLogger(TestNewsDatabase.class);
+	
+	/**
+	 * Sorted by latitude.
+	 */
+	private List<News> newsList;
+	
+	public TestNewsDatabase(String dbPath, NewsReader newsReader) {
+		logger.info(dbPath);
+		
+		File dbf = new File(dbPath);
+		if (dbf.exists()) {
+			/*
+			 * Use this one
+			 */
+			try {
+				ObjectInputStream ois = new ObjectInputStream(new FileInputStream(dbf));
+				newsList = (List<News>) ois.readObject();
+				ois.close();
+			} catch (FileNotFoundException e) {
+				logger.error(e);
+			} catch (IOException e) {
+				logger.error(e);
+			} catch (ClassNotFoundException e) {
+				logger.error(e);
+			}			
+		} else {
+			logger.error(dbPath + " does not exist.");
+//			/*
+//			 * Create one
+//			 */
+//			newsList = newsReader.read();
+//			/*
+//			 * Sort by latitude
+//			 */
+//			Collections.sort(newsList, latitudeComparator);
+//			try {
+//				ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(dbf));
+//				oos.writeObject(newsList);
+//				oos.close();
+//			} catch (FileNotFoundException e) {
+//				logger.error(e);
+//			} catch (IOException e) {
+//				logger.error(e);
+//			}			
+		}
+	}
+
+	public int size() {
+		return this.newsList.size();
+	}
+	
+	@Override
+	public List<News> query(List<float[]> waypoints) {
+		return lookUpInList(waypoints, newsList);
+	}
+	
+}
